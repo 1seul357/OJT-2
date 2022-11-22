ee901fb70df0e2fdfc2e656dc51ae48ea0fe71f8
1seul357 <1seul357@gmail.com>
2022-11-22 14:56:44 +0900

    Refactor: 코드 리팩터링

--- src/Index.js
+++ src/Index.js
@@ -10,21 +10,21 @@ export default class Index {
   }
 
   render() {
-    var draw = SVG().addTo(this.section).size(800, 800);
-    var rect = draw
+    let draw = SVG().addTo(this.section).size(800, 800);
+    let rect = draw
       .rect(150, 150)
       .x(100)
       .y(100)
       .addClass("rect")
       .attr({ fill: "#3e5f97" });
-    var circle = draw.circle(150).x(250).y(250).attr({ fill: "#f4c17b" });
+    let circle = draw.circle(150).x(250).y(250).attr({ fill: "#f4c17b" });
     let flag = true;
+    const group = draw.group();
 
     // 사각형 클릭하면 꼭지점 나타남, 그룹화해서 드래그 시 같이 움직일 수 있도록 한 코드
     rect.click(function (e) {
       if (flag == true) {
         const box = rect.bbox();
-        const group = draw.group();
         group.add(rect);
 
         const array = [
@@ -36,13 +36,14 @@ export default class Index {
 
         for (let i = 0; i < array.length; i++) {
           const circle = draw
-            .circle(8)
+            .circle(10)
             .x(array[i][0])
             .y(array[i][1])
             .addClass("vertex")
             .attr({ fill: "black" });
           circle.click(function (e) {
-            console.log(123); // 클릭 후 크기 조절할 수 있게 만들기
+            console.log(rect); // 클릭 후 크기 조절할 수 있게 만들기
+            circle.style.cursor = point;
           });
           group.add(circle);
         }
@@ -55,7 +56,11 @@ export default class Index {
 
     // 사각형 제외 다른 영역 클릭 시 꼭지점 제거
     this.section.addEventListener("click", function (e) {
-      if (e.target == document.querySelector(".rect")) {
+      if (
+        e.target == document.querySelector(".rect") ||
+        e.target.className.animVal ==
+          document.querySelector(".vertex").className.animVal
+      ) {
         return;
       }
       flag = true;
