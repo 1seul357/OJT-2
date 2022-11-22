8712351ffe09d78ff2d0eba014ed72af4ad82434
1seul357 <1seul357@gmail.com>
2022-11-22 13:51:21 +0900

    Feat: vertex 추가, 그룹화

--- src/Index.js
+++ src/Index.js
@@ -1,5 +1,6 @@
 import { SVG } from "@svgdotjs/svg.js";
 import "@svgdotjs/svg.draggable.js";
+import "../src/Index.css";
 
 export default class Index {
   constructor({ $target }) {
@@ -10,15 +11,9 @@ export default class Index {
   }
   render() {
     var draw = SVG().addTo(this.section).size(800, 800);
-    var rect = draw.rect(150, 150).attr({ fill: "#3e5f97" });
-
-    var circle = draw.circle(150).x(200).y(200).attr({ fill: "#f4c17b" });
-
-    var polygon = draw
-      .polygon("50, 0 60, 40 100, 50 60, 60 50, 100 40, 60 0, 50 40, 40")
-      .x(500)
-      .y(200);
-    polygon.fill("#f06");
+    var rect = draw.rect(150, 150).x(100).y(100).attr({ fill: "#3e5f97" });
+    var circle = draw.circle(150).x(250).y(250).attr({ fill: "#f4c17b" });
+    let flag = true;
 
     const dragItem = (e) => {
       const { handler, box } = e.detail;
@@ -26,16 +21,36 @@ export default class Index {
       handler.move(box.x, box.y);
     };
 
-    rect.draggable().on("dragmove", (e) => {
-      dragItem(e);
-    });
-
     circle.draggable().on("dragmove", (e) => {
       dragItem(e);
     });
 
-    polygon.draggable().on("dragmove", (e) => {
-      dragItem(e);
+    rect.click(function (e) {
+      if (flag == true) {
+        const box = rect.bbox();
+        const group = draw.group();
+        group.add(rect);
+
+        const array = [
+          [box.x - 2, box.y - 2],
+          [box.x2 - 6, box.y - 2],
+          [box.x - 2, box.y2 - 6],
+          [box.x2 - 6, box.y2 - 6],
+        ];
+
+        for (let i = 0; i < array.length; i++) {
+          const circle = draw
+            .circle(8)
+            .x(array[i][0])
+            .y(array[i][1])
+            .attr({ fill: "black" });
+          group.add(circle);
+        }
+        group.draggable().on("dragmove", (e) => {
+          dragItem(e);
+        });
+      }
+      flag = false;
     });
   }
 }
