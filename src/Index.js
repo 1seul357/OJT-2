a3997f5259a88e814394d62f3dfe7c7488726aa2
1seul357 <1seul357@gmail.com>
2022-11-22 14:29:22 +0900

    Feat: 도형 제외 영역 클릭 시 꼭지점 제거

--- src/Index.js
+++ src/Index.js
@@ -1,6 +1,5 @@
 import { SVG } from "@svgdotjs/svg.js";
 import "@svgdotjs/svg.draggable.js";
-import "../src/Index.css";
 
 export default class Index {
   constructor({ $target }) {
@@ -9,22 +8,19 @@ export default class Index {
     this.render();
     $target.appendChild(this.section);
   }
+
   render() {
     var draw = SVG().addTo(this.section).size(800, 800);
-    var rect = draw.rect(150, 150).x(100).y(100).attr({ fill: "#3e5f97" });
+    var rect = draw
+      .rect(150, 150)
+      .x(100)
+      .y(100)
+      .addClass("rect")
+      .attr({ fill: "#3e5f97" });
     var circle = draw.circle(150).x(250).y(250).attr({ fill: "#f4c17b" });
     let flag = true;
 
-    const dragItem = (e) => {
-      const { handler, box } = e.detail;
-      e.preventDefault();
-      handler.move(box.x, box.y);
-    };
-
-    circle.draggable().on("dragmove", (e) => {
-      dragItem(e);
-    });
-
+    // 사각형 클릭하면 꼭지점 나타남, 그룹화해서 드래그 시 같이 움직일 수 있도록 한 코드
     rect.click(function (e) {
       if (flag == true) {
         const box = rect.bbox();
@@ -43,7 +39,11 @@ export default class Index {
             .circle(8)
             .x(array[i][0])
             .y(array[i][1])
+            .addClass("vertex")
             .attr({ fill: "black" });
+          circle.click(function (e) {
+            console.log(123); // 클릭 후 크기 조절할 수 있게 만들기
+          });
           group.add(circle);
         }
         group.draggable().on("dragmove", (e) => {
@@ -51,6 +51,26 @@ export default class Index {
         });
       }
       flag = false;
+    });
+
+    // 사각형 제외 다른 영역 클릭 시 꼭지점 제거
+    this.section.addEventListener("click", function (e) {
+      if (e.target == document.querySelector(".rect")) {
+        return;
+      }
+      flag = true;
+      document.querySelectorAll(".vertex").forEach((node) => node.remove());
+    });
+
+    // 드래그 함수
+    const dragItem = (e) => {
+      const { handler, box } = e.detail;
+      e.preventDefault();
+      handler.move(box.x, box.y);
+    };
+
+    circle.draggable().on("dragmove", (e) => {
+      dragItem(e);
     });
   }
 }
