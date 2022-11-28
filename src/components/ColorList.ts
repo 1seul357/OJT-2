103356f4784a40d0af7a1c21276dfdef20ac1ad5
1seul357 <1seul357@gmail.com>
2022-11-28 10:01:37 +0900

    Feat: 컬러 추가

--- src/components/ColorList.ts
+++ src/components/ColorList.ts
@@ -14,9 +14,12 @@ export default class colorList {
       "#FFC6E0",
       "#f4c17b",
       "#EDAA7D",
+      "#fffb7a",
       "#77af9c",
       "#96B1D0",
+      "#d8ceff",
       "#C8707E",
+      "#ff4f70",
     ];
 
     const container = document.createElement("div");
